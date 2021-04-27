2bff56a1f47433d8c94d1d2db0d58e332735f72e
leoworkcp <leoworkcp@gmail.com>
2021-04-27 02:04:28 -0400

    UI set up for liked songs Page

--- react-app/src/components/LikedSongs/index.js
+++ react-app/src/components/LikedSongs/index.js
@@ -11,10 +11,9 @@ const LikedSongs = ({ loggedInUser, authenticated, setAuthenticated }) => {
   return (
     <div className="liked_page__container">
       <div className="liked_page__banner">
-        {/*  */}
         <div className="container-ref">
           <img src={likedImg}></img>
-          {/*  */}
+
           <div className="liked_page__info">
             <div className="liked-user__info">
               <h2>PLAYLIST</h2>
@@ -27,6 +26,37 @@ const LikedSongs = ({ loggedInUser, authenticated, setAuthenticated }) => {
                 <p>{` • ${1223} songs`}</p>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="play-liked__container">
+        <div className="liked-btn__container"></div>
+      </div>
+      <div className="song-liked__container">
+        <div className="liked-grid__container">
+          <div role="presentation" className="presentation-container">
+            <div className="presentation__row"># </div>
+            <div className="presentation__row">TITLE</div>
+            <div className="presentation__row margin-move">ALBUM</div>
+            <div className="presentation__row">DATE ADDED</div>
+            <div className="presentation__row">⌚</div>
+          </div>
+          <div role="presentation" className="presentation-container">
+            <div className="presentation__row">1</div>
+            <div className="presentation__img">
+              <div id="img_liked_container">
+                <img id="img_liked-songs" src={loggedInUser.profile_URL}></img>
+              </div>
+              <div className="info-links__artist">
+                <p> goosePoon</p>
+                <NavLink id="artist_linked" to={`/profile/${userId}`}>
+                  {loggedInUser.username}
+                </NavLink>
+              </div>
+            </div>
+            <div className="presentation__row">Track Star</div>
+            <div className="presentation__row">11 days ago</div>
+            <div className="presentation__row">3:28</div>
           </div>
         </div>
       </div>
